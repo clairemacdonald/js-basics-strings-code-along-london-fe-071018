a9d6ed2f801bf5326f63a71fd5035163402c1d1c
clairemacdonald <claire.macdonald@blis.com>
2018-08-07 21:27:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Claire';
+ 
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
